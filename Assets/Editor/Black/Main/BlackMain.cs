4d68865776672633ca76235b3351f0978610a99f
youarebritish <bishop@projectbc.net>
2020-05-08 15:12:52 -0700

    Added initialization of object types on load

---
+++ Assets/Editor/Black/Main/BlackMain.cs
@@ -0,0 +1,18 @@
+﻿using Black.Entity;
+
+namespace Black.Main
+{
+    [UnityEditor.InitializeOnLoad]
+    public class BlackMain
+    {
+        static BlackMain()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            EntityModuleSetup.Setup();
+        }
+    }
+}
