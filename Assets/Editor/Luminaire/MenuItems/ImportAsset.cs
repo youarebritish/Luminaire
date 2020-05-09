331e339e26da9afc42216a7f2b80f0008f2834f6
youarebritish <bishop@projectbc.net>
2020-05-09 14:56:35 -0700

    Import path now uses the package's sourcepath

--- Assets/Editor/Luminaire/MenuItems/ImportAsset.cs
+++ Assets/Editor/Luminaire/MenuItems/ImportAsset.cs
@@ -1,61 +1,55 @@
-﻿using Luminaire.Entity;
-using SQEX.Ebony.Framework.Entity;
-using System.IO;
-using System.Text;
-using UnityEditor;
-using UnityEngine;
-
-public static class ImportAsset
+﻿
+namespace Luminaire.MenuItems
 {
-    [MenuItem("Luminaire/Import Entity Package")]
-    private static void OnImportAsset()
+    using Luminaire.Entity;
+    using SQEX.Ebony.Framework.Entity;
+    using System.IO;
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class ImportAsset
     {
-        // TODO pull out into a utility function
-        var settings = AssetDatabase.LoadAssetAtPath<LuminaireSettings>(LuminaireSettings.SettingsPath);
-        var gamePath = settings.GamePath;
-        if (string.IsNullOrEmpty(gamePath))
+        private const string ImportPath = "Assets/Mod/";
+
+        [MenuItem("Luminaire/Import Entity Package")]
+        private static void OnImportAsset()
         {
-            EditorUtility.DisplayDialog("Game path not set", "Game path must be set before assets can be imported.", "OK");
-            return;
+            // TODO pull out into a utility function
+            var settings = AssetDatabase.LoadAssetAtPath<LuminaireSettings>(LuminaireSettings.SettingsPath);
+            var gamePath = settings.GamePath;
+            if (string.IsNullOrEmpty(gamePath))
+            {
+                EditorUtility.DisplayDialog("Game path not set", "Game path must be set before assets can be imported.", "OK");
+                return;
+            }
+
+            if (!File.Exists(gamePath))
+            {
+                EditorUtility.DisplayDialog("Invalid game path", "Invalid game path. Make sure the game path points to a valid game executable.", "OK");
+                return;
+            }
+
+            var assetPath = EditorUtility.OpenFilePanel("Import asset", gamePath, "exml");
+            if (string.IsNullOrEmpty(assetPath))
+            {
+                return;
+            }
+
+            var fileContent = File.ReadAllBytes(assetPath);
+            var loader = new EntityPackageXmlLoader();
+            var package = loader.CreateEntityPackage(fileContent);
+
+            var packageAsset = ScriptableObject.CreateInstance<EntityPackageAsset>();
+
+            var destinationPath = MakeDestinationPath(package.sourcePath_);
+            Directory.CreateDirectory(destinationPath);
+
+            AssetDatabase.CreateAsset(packageAsset, destinationPath);
+            AssetDatabase.SaveAssets();
+
+            packageAsset.Package = package;
         }
 
-        if (!File.Exists(gamePath))
-        {
-            EditorUtility.DisplayDialog("Invalid game path", "Invalid game path. Make sure the game path points to a valid game executable.", "OK");
-            return;
-        }
-
-        var assetPath = EditorUtility.OpenFilePanel("Import asset", gamePath, "exml");
-        if (string.IsNullOrEmpty(assetPath))
-        {
-            return;
-        }
-
-        var fileContent = File.ReadAllBytes(assetPath);
-        var loader = new EntityPackageXmlLoader();
-        var package = loader.CreateEntityPackage(fileContent);
-
-        var packageAsset = ScriptableObject.CreateInstance<EntityPackageAsset>();
-
-        var destinationPath = MakeDestinationPath(assetPath, gamePath);
-        Directory.CreateDirectory(destinationPath);
-
-        AssetDatabase.CreateAsset(packageAsset, destinationPath);
-        AssetDatabase.SaveAssets();
-
-        packageAsset.Package = package;
-    }
-
-    private static string MakeDestinationPath(string assetPath, string gamePath)
-    {
-        // TODO use the package's source path
-        var stringBuilder = new StringBuilder();
-        stringBuilder.Append("Assets/");
-
-        var relativePath = assetPath.Substring(Path.GetDirectoryName(gamePath).Length);
-        stringBuilder.Append(relativePath);
-        stringBuilder.Append(".asset");
-
-        return stringBuilder.ToString();
+        private static string MakeDestinationPath(string sourcePath) => $"{ImportPath}{sourcePath}.asset";
     }
 }
