e015d7015b72d50f27ec645d37aaed4a5b366b8b
youarebritish <bishop@projectbc.net>
2020-05-08 21:40:10 -0700

    More code cleanup

--- Assets/Editor/Luminaire/ClassGeneration/ClassGenerator.cs
+++ Assets/Editor/Luminaire/ClassGeneration/ClassGenerator.cs
@@ -1,20 +1,38 @@
 ﻿namespace Luminaire.ClassGeneration
 {
+    using Newtonsoft.Json;
+    using Scriban;
+    using Scriban.Runtime;
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using Newtonsoft.Json;
-    using Scriban;
-    using Scriban.Runtime;
     using UnityEngine.Assertions;
     using static SQEX.Luminous.Core.Object.Property;
 
+    /// <summary>
+    /// Generates C# class files for Luminous engine type data.
+    /// </summary>
     public static class ClassGenerator
     {
+        /// <summary>
+        /// Output directory for the generated files.
+        /// </summary>
         private const string OutputDirectory = "/Editor/Generated/";
+
+        /// <summary>
+        /// Suffix for the generated files.
+        /// </summary>
         private const string GeneratedFileSuffix = ".generated.cs";
+
+        /// <summary>
+        /// Name for the object setup class.
+        /// </summary>
         private const string SetupClassTypeName = "Black.Entity.EntityModuleSetup";
+
+        /// <summary>
+        /// Names of types to not generated, perhaps because they're problematic, manually implemented, or merely annoying.
+        /// </summary>
         private static readonly IReadOnlyCollection<string> TypesToSkip = new SortedSet<string>
         {
         "BaseObject",
@@ -37,39 +55,60 @@
         "Black.Save.Multiplay.SavePlantMapDataStruct.IconDataStruct"
         };
 
+        /// <summary>
+        /// Serialized type metadata.
+        /// </summary>
         private class SerializedObjectType
         {
             [JsonProperty("name_")]
             public string Name { get; set; }
+
             [JsonProperty("thistype")]
             public uint ThisType { get; set; }
+
             [JsonProperty("basetype")]
             public string BaseType { get; set; }
+
+            public bool HasBaseType => !string.IsNullOrEmpty(this.BaseType);
+
             [JsonProperty("constructFunction_")]
             public ulong ConstructFunction { get; set; }
+
             [JsonProperty("constructFunction2_")]
             public ulong ConstructFunction2 { get; set; }
+
             public bool HasConstructor => this.ConstructFunction2 != 0;
+
             [JsonProperty("singletonFunction_")]
             public ulong SingletonFunction { get; set; }
+
             [JsonProperty("propertyContainer_")]
             public SerializedPropertyContainer PropertyContainer { get; set; }
+
             [JsonProperty("functionCount_")]
             public uint FunctionCount { get; set; }
+
             [JsonProperty("objectSize_")]
             public int ObjectSize { get; set; }
         }
 
+        /// <summary>
+        /// Serialized property container metadata.
+        /// </summary>
         private class SerializedPropertyContainer
         {
             [JsonProperty("nameHashCode_")]
             public int NameHashCode { get; set; }
+
             [JsonProperty("versionHashCode_")]
             public int VersionHashCode { get; set; }
+
             [JsonProperty("allPropertiesClassFieldCount_")]
             public ushort AllPropertiesClassFieldCount { get; set; }
+
             [JsonProperty("myProperties_")]
             public SerializedProperty[] MyProperties { get; set; }
+
             [JsonProperty("allProperties_")]
             public SerializedProperty[] AllProperties { get; set; }
 
@@ -78,24 +117,36 @@
                                                                           select property;
         }
 
+        /// <summary>
+        /// Serialized property metadata.
+        /// </summary>
         private class SerializedProperty
         {
             [JsonProperty("name_")]
             public string Name { get; set; }
+
             public string ValidName => this.NameValid;
+
             [JsonProperty("nameHash_")]
             public uint NameHash { get; set; }
+
             [JsonProperty("typeName_")]
             public string TypeName { get; set; }
+
             public string TargetTypeName => this.GetTargetTypeName();
+
             [JsonProperty("offset_")]
             public uint Offset { get; set; }
+
             [JsonProperty("size_")]
             public uint Size { get; set; }
+
             [JsonProperty("itemCount_")]
             public ushort ItemCount { get; set; }
+
             [JsonProperty("primitiveType_")]
             public PrimitiveType PrimitiveType { get; set; }
+
             public string PrimitiveTypeName => this.PrimitiveType.ToString();
 
             [JsonProperty("attr_")]
@@ -252,6 +303,12 @@
             }
         }
 
+        /// <summary>
+        /// Generate C# class files for the provided object type data.
+        /// </summary>
+        /// <param name="schema">The object type data in JSON format.</param>
+        /// <param name="classTemplate">Template for the class files.</param>
+        /// <param name="setupTemplate">Template for the class setup file.</param>
         public static void GenerateClasses(string schema, string classTemplate, string setupTemplate)
         {
             Assert.IsNotNull(schema);
@@ -306,18 +363,6 @@
                                                          let typeTokens = objectTypeData.Name.Split('.')
                                                          select (objectTypeData, typeTokens))
             {
-                // FIXME: Make it more clear that we're doing this to skip some problematic types
-                if (typeTokens.Length < 2)
-                {
-                    continue;
-                }
-
-                // FIXME: Instead of this dumb hack, add a check for this in the template
-                if (string.IsNullOrEmpty(objectTypeData.BaseType))
-                {
-                    objectTypeData.BaseType = null;
-                }
-
                 var typeNamespace = ExtractNamespace(typeTokens);
                 var type = typeTokens[typeTokens.Length - 1];
 
